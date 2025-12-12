a9f6f757373ab37e5af48b15a96206ebb842dc33
maedi <maediprichard@gmail.com>
2025-12-13 09:17:24 +1100

    Rename Observatory to ObservationDeck

--- lib/observers.rb
+++ lib/observers.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-require_relative '../observatory'
+require_relative '../observation_deck'
 
 module Observers
   def observable(observable = nil)
     observable = self if observable.nil?
-    Observatory.track(observable:)
+    ObservationDeck.track(observable:)
   end
 
   def observe(observable)
-    Observatory.observe(observable:, observer: self)
+    ObservationDeck.observe(observable:, observer: self)
   end
 end
