213e0a57293ba4073c936e4466b125f527fe9f75
maedi <maediprichard@gmail.com>
2025-12-13 15:33:50 +1100

    Rename Publisher to Observable

--- spec/features/observable_spec.rb
+++ spec/features/observable_spec.rb
@@ -1,23 +1,21 @@
 # frozen_string_literal: true
 
-require_relative '../fixtures/publisher'
+require_relative '../fixtures/observable'
 
-RSpec.describe Publisher do
-  subject(:publisher) { described_class }
-
+RSpec.describe Observable do
   before do
     Observers::Observations.reset
-    Object.send(:remove_const, 'Publisher')
-    load 'spec/fixtures/publisher.rb'
+    Object.send(:remove_const, 'Observable')
+    load 'spec/fixtures/observable.rb'
   end
 
   describe '#initialize' do
     it 'instantiates a class' do
-      expect { publisher }.not_to raise_error
+      expect { Observable }.not_to raise_error
     end
 
     it 'creates an observation' do
-      publisher
+      Observable
       expect(Observers::Observations.observations.count).to eq(1)
     end
   end
--- spec/fixtures/observable.rb
+++ spec/fixtures/observable.rb
@@ -1,6 +1,6 @@
 require_relative '../../lib/observers'
 
-class Publisher
+class Observable
   extend Observers
 
   observable
