e17dc21c2b4c29eedb33158a2fac4724bfae6c1a
maedi <maediprichard@gmail.com>
2025-12-13 15:19:55 +1100

    Test creating an observation

--- spec/features/publisher_spec.rb
+++ spec/features/publisher_spec.rb
@@ -3,11 +3,22 @@
 require_relative '../fixtures/publisher'
 
 RSpec.describe Publisher do
-  subject(:publisher) { described_class.new }
+  subject(:publisher) { described_class }
+
+  before do
+    Observers::Observations.reset
+    Object.send(:remove_const, 'Publisher')
+    load 'spec/fixtures/publisher.rb'
+  end
 
   describe '#initialize' do
     it 'instantiates a class' do
       expect { publisher }.not_to raise_error
+    end
+
+    it 'creates an observation' do
+      publisher
+      expect(Observers::Observations.observations.count).to eq(1)
     end
   end
 end
