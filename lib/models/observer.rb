6de59191528bd1e73e3afc418f623a5e1d7017e8
maedi <maediprichard@gmail.com>
2026-09-06 22:36:56 +1000

    Rename route to route_request

--- lib/models/observer.rb
+++ lib/models/observer.rb
@@ -24,8 +24,8 @@ module Observers
 
       raise ArgumentError, "#{type}##{action} has an 'event:' keyword argument but no event arg was sent" if event.nil?
 
-      # Events trigger events, so the error bubbles up to becomes the error message for the next rescue's error message:
-      # "RequestEvent sent to Rain::Router#route -> StatusEvent sent to Error404Node.render -> unknown keyword: :props"
+      # Events trigger events, so the error bubbles up to become the error message for the next rescue's error message:
+      # "RequestEvent sent to Rain::Router#route_request -> StatusEvent sent to Error404Node.render -> unknown keyword: :props"
       raise ArgumentError, "#{event.class} sent to #{type}#{method_type}#{action} -> #{e.message}"
     end
   end
